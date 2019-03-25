6ae8049cd737d3281a824c8c933df4e51eada93b
Mitchell <davmitchell@AU10571.local>
2019-03-25 18:09:06 +1100

    Now fully working locally

---
+++ README.md
@@ -0,0 +1,5 @@
+To simulate call to Lambda function locally
+`$ ./node_modules/lambda-local/bin/lambda-local -l handler.js -e event.json -h handler`
+
+To run cucumber locally
+`$ ./node_modules/.bin/cucumber-js --format json`
--- handler.js
+++ handler.js
@@ -10,9 +10,10 @@ const s3bucket = process.env.S3_BUCKET_NAME || ''
 console.log('Reading step files from ' + s3bucket)
 
 module.exports.handler = async (event, context) => {
+  console.log('event: ' + JSON.stringify(event))
   if (s3bucket !== '') {
     // Copy step files from S3 bucket to Lambda's own filesystem
-    const s3files = s3filelist(s3bucket)
+    const s3files = await s3FileList(s3bucket)
     copyFromS3(s3bucket, s3files)
   }
 
@@ -24,9 +25,9 @@ module.exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      // message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
-      output: cucumberResult
+      output: JSON.parse(cucumberResult)
     })
   }
 
@@ -36,23 +37,44 @@ module.exports.handler = async (event, context) => {
   // TODO: Return http 501 (Not Implemented) if Cucumber steps haven't been implemented yet
 }
 
-const s3filelist = (s3bucket) => {
-  var fileList = []
-  S3.listObjectsV2({ Bucket: s3bucket, MaxKeys: 1000 }).forEach(element => {
-    fileList.append(element.Key)
-  })
+const s3FileList = async (s3bucket) => {
+  // var fileList = []
+  console.log('Step and environment files are held in bucket ' + s3bucket)
+  // S3.listObjectsV2({ Bucket: s3bucket, MaxKeys: 1000 }).forEach(element => {
+  //   fileList.append(element.Key)
+  // })
+  var fileList = await getFiles({ Bucket: s3bucket })
+  console.log('S3 filelist: ' + fileList)
   return fileList
+}
+
+const getFiles = async (params) => {
+  const response = await S3.listObjectsV2(params).promise()
+  console.log('S3 bucket contents: ' + JSON.stringify(response))
+  var keys = []
+  response.Contents.forEach(obj => {
+    if (obj.Key.slice(-1) !== '/') {
+      keys.push(obj.Key)
+    }
+  })
+  if (response.IsTruncated) {
+    const newParams = Object.assign({}, params)
+    newParams.ContinuationToken = response.NextContinuationToken
+    await getFiles(newParams, keys)
+  }
+  return keys
 }
 
 const copyFromS3 = (s3bucket, fileList) => {
   const fs = require('fs')
   const path = require('path')
+  console.log(fileList)
   fileList.forEach(file => {
     var params = {
       Bucket: s3bucket,
       Key: file
     }
-    var localFilename = path.join('/tmp/', file)
+    var localFilename = path.join('./features/support/', file)
     var localFile = fs.createWriteStream(localFilename)
     S3.getObject(params).createReadStream().pipe(localFile)
   })
@@ -62,15 +84,21 @@ const saveEventFeatureToFile = (event) => {
   const fs = require('fs')
   const tmp = require('tmp')
   const tmpobj = tmp.fileSync({ postfix: '.feature' })
-  const featureJSON = fs.readFileSync('./event.json')
-  const feature = JSON.parse(featureJSON).feature
+  var feature
+  if (event == null) {
+    const featureJSON = fs.readFileSync('./event.json')
+    feature = JSON.parse(featureJSON).feature
+  } else {
+    console.log('Received event: ' + event)
+    feature = event.feature
+  }
   console.log('Feature: ' + feature)
   fs.writeFileSync(tmpobj.name, feature, 'utf8')
   return tmpobj.name
 }
 
 const executeCucumber = (featureFilename) => {
-  const shellCmd = './node_modules/.bin/cucumber-js ' + featureFilename
+  const shellCmd = './node_modules/.bin/cucumber-js ' + featureFilename + ' --format json --require "./features/**/*.js"'
   var response = shell.exec(shellCmd)
   return response
 }
