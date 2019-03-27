0f280647b9a6690c51e04c8a31247070591aec33
Mitchell <davmitchell@AU10571.local>
2019-03-27 11:41:42 +1100

    Bug hunt

--- handler.js
+++ handler.js
@@ -10,7 +10,7 @@ const s3bucket = process.env.S3_BUCKET_NAME || ''
 console.log('Reading step files from ' + s3bucket)
 
 module.exports.handler = async (event, context) => {
-  console.log('event: ' + JSON.stringify(event))
+  console.log('Received event: ' + JSON.stringify(event))
   if (event.body === undefined) {
     return {
       statusCode: 400,
@@ -18,9 +18,11 @@ module.exports.handler = async (event, context) => {
       result: "Can't parse event.body.feature"
     }
   }
+  var s3StepDefinitionFiles
   if (s3bucket !== '') {
     // Copy step files from S3 bucket to Lambda's own filesystem
-    const s3StepDefinitionFiles = await s3StepDefinitionFileList(s3bucket)
+    s3StepDefinitionFiles = await s3StepDefinitionFileList(s3bucket)
+    console.log('Found the following S3 StepDefinitionFiles: ' + s3StepDefinitionFiles)
     copyStepDefinitionsFromS3(s3bucket, s3StepDefinitionFiles)
   }
 
@@ -30,11 +32,20 @@ module.exports.handler = async (event, context) => {
   const cucumberResult = executeCucumber(featureFilename)
   // console.log('cucumberResult: ' + cucumberResult)
 
-  if (cucumberResult == '') {
+  // Remove both the saved feature file and the step files copied from S3, so they don't pollute
+  // the file space for subsequent executions of this Lambda function
+  console.log('cucumberResult...')
+  console.log(cucumberResult)
+  tidyUpFiles(featureFilename)
+  if (s3StepDefinitionFiles !== '') {
+    tidyUpFiles(s3StepDefinitionFiles)
+  }
+
+  if (cucumberResult.code === 1) {
     return {
       statusCode: 501,
       feature: event.body.feature,
-      result: 'Cucumber returned no result - possibly missing step files?'
+      result: JSON.parse(cucumberResult)
     }
   }
   return {
@@ -54,9 +65,9 @@ module.exports.handler = async (event, context) => {
 
 const s3StepDefinitionFileList = async (s3bucket) => {
   // console.log('Step and environment files are held in bucket ' + s3bucket)
-  var fileList = await getFiles({ Bucket: s3bucket })
-  console.log('S3 filelist: ' + fileList)
-  return fileList
+  var s3StepFiles = await getFiles({ Bucket: s3bucket })
+  // console.log('S3 step files: ' + s3StepFiles)
+  return s3StepFiles
 }
 
 const getFiles = async (params) => {
@@ -85,46 +96,48 @@ const copyStepDefinitionsFromS3 = (s3bucket, fileList) => {
       Bucket: s3bucket,
       Key: file
     }
-    var localFilename = path.join('features/step-definitions/', file)
+    var localFilename = path.join('/tmp/step-definitions/', file)
     console.log('Copying steps from s3://' + s3bucket + '/' + file + ' to local file ' + localFilename)
     var localFile = fs.createWriteStream(localFilename)
+    console.log('Localfile details: ' + JSON.stringify(localFile))
     const fileResult = S3.getObject(params).createReadStream().pipe(localFile)
     console.log('File copy result: ' + JSON.stringify(fileResult))
+    const localFileContent = shell.exec('cat ' + localFile)
+    console.log('localFileContent: ' + localFileContent)
   })
 }
 
 const saveEventFeatureToFile = (event) => {
   const fs = require('fs')
-  const tmp = require('tmp')
-  const tmpobj = tmp.fileSync({ postfix: '.feature' })
+  const localFeatureFilename = '/tmp/input.feature'
   var feature
-  // console.log('event to process: ' + JSON.stringify(event))
   if (event == null) {
     // No event supplied - read a fake one from ./event.json and use that
     const featureJSON = fs.readFileSync('./event.json')
-    // console.log('featureJSON: ' + featureJSON)
     feature = JSON.parse(featureJSON).body.feature
-    // console.log('feature: ' + feature)
   } else {
     // Pull apart the event received from API Gateway
-    // console.log('Received event: ' + JSON.stringify(event))
-    // console.log('Received event.body: ' + JSON.stringify(event.body))
-    // feature = JSON.stringify(event.body.feature)
     feature = event.body.feature
-    // console.log('feature from event: ' + feature)
   }
-  console.log('Feature: ' + feature)
-  fs.writeFileSync(tmpobj.name, feature, 'utf8')
-  return tmpobj.name
+  // console.log('Feature: ' + feature)
+  fs.writeFileSync(localFeatureFilename, feature, 'utf8')
+  // console.log('Saved input feature to ' + localFeatureFilename)
+  const featureFileContent = shell.exec('cat ' + localFeatureFilename)
+  console.log(featureFileContent)
+  return localFeatureFilename
 }
 
 const executeCucumber = (featureFilename) => {
-  const dirFiles = 'ls -lR features/'
+  const dirFiles = 'ls -lR /tmp/'
   const files = shell.exec(dirFiles)
   console.log('files: ' + files)
-  const shellCmd = './node_modules/.bin/cucumber-js ' + featureFilename + ' --format json --require "./features/step-definitions/*.js"'
+  const shellCmd = './node_modules/.bin/cucumber-js ' + featureFilename + ' --format json --require "/tmp/step-definitions/*.js"'
   console.log('Cucumber request: ' + shellCmd)
   const response = shell.exec(shellCmd)
   // console.log('Cucumber response: ' + response)
   return response
+}
+
+const tidyUpFiles = (localFilename) => {
+  shell.exec('rm ' + localFilename)
 }
