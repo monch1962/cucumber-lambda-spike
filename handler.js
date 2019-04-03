3227fba5a36741d7ac84b61c9fa2b86cf487614b
Mitchell <davmitchell@AU10571.local>
2019-04-04 00:46:31 +1100

    Bug fixes

--- handler.js
+++ handler.js
@@ -28,29 +28,32 @@ module.exports.handler = async (event, context) => {
   var s3StepFiles
   if (s3bucket !== '') {
     // Copy step files from S3 bucket to Lambda's own filesystem
-    // s3StepFiles = await s3StepFileList(s3bucket)
-    // console.log('Found the following S3 StepDefinitionFiles: ' + s3StepFiles)
-    // copyStepFilesFromS3(s3bucket, s3StepFiles)
+    s3StepFiles = await s3StepFileList(s3bucket)
+    console.log('Found the following S3 StepDefinitionFiles: ' + s3StepFiles)
+    await copyStepFilesFromS3(s3bucket, s3StepFiles)
   }
 
   const featureFilename = saveEventFeatureToFile(event)
 
-  const cucumberResult = executeCucumber(featureFilename)
+  const cucumberResult = await executeCucumber(featureFilename)
+  console.log('cucumberResult: \n' + cucumberResult.toString())
   removeFile(featureFilename)
   if (s3StepFiles !== undefined) {
-    // console.log('s3StepFiles: ' + s3StepFiles)
-    // console.log('s3StepFiles.split(): ' + s3StepFiles.toString().split(','))
-    // const tmpFiles = listFiles('/tmp')
-    // console.log(tmpFiles)
-    s3StepFiles.forEach(stepFile => {
-      // removeFile(stepFile)
-    })
+    console.log('s3StepFiles: ' + s3StepFiles)
+    console.log('s3StepFiles.split(): ' + s3StepFiles.toString().split(','))
+    const tmpFiles = listFiles('/tmp')
+    console.log(tmpFiles)
+    for (const stepFile in s3StepFiles) {
+      removeFile(stepFile)
+    }
   }
 
   var statusCode = 200
   if (cucumberResult.status === 1) {
     statusCode = 501
   }
+
+  console.log('cucumberResult.output: \n' + JSON.stringify(cucumberResult))
   const response = {
     statusCode: statusCode,
     headers: {
@@ -66,9 +69,7 @@ module.exports.handler = async (event, context) => {
 }
 
 const s3StepFileList = async (s3bucket) => {
-  // console.log('Step and environment files are held in bucket ' + s3bucket)
   const s3StepFiles = await getFiles({ Bucket: s3bucket })
-  // console.log('S3 step files: ' + s3StepFiles)
   return s3StepFiles
 }
 
@@ -93,34 +94,39 @@ const getFiles = async (params) => {
 }
 
 const copyStepFilesFromS3 = async (s3bucket, fileList) => {
-  const fs = require('fs')
+  var fs = require('fs')
   const path = require('path')
 
-  fileList.forEach(file => {
+  for (const file of fileList) {
     const params = {
       Bucket: s3bucket,
       Key: file.toString()
     }
-
-    // console.log(S3.getObject(params))
-    // console.log('typeof file: ' + typeof file)
-    const localFilename = path.join('/tmp/step-definitions/', file)
+    const localFilename = path.join('/tmp/step-definitions/', file.toString())
+    // const localFilename = '/tmp/step-definitions/' + file.toString()
 
     console.log('Copying from s3://' + s3bucket + '/' + file + ' to local file ' + localFilename)
-    const localFile = fs.createWriteStream(localFilename)
-    S3.getObject(params).createReadStream().pipe(localFile)
 
-    // S3.getObject(params, (err, data) => {
-    //   console.log('File data: ' + data.Body.toString('utf-8'))
-    //   if (err) throw (err)
+    const localDir = path.parse(localFilename).dir
+    if (!fs.existsSync(localDir)) {
+      fs.mkdirSync(localDir)
+    }
 
-    //   fs.writeFileSync(data.Body.toString('utf-8'))
-    //   console.log(`${localFilename} has been created`)
-    // })
+    const fileData = getS3Object(params)
+    fs.writeFileSync(localFilename, fileData, 'utf8')
+    console.log('/tmp files: \n' + listFiles('/tmp'))
+  }
+  const tmpFiles = listFiles('/tmp')
+  console.log('/tmp files: \n' + tmpFiles)
+}
+
+const getS3Object = (handle) => {
+  return new Promise((resolve, reject) => {
+    S3.getObject(handle, (err, data) => {
+      if (err) reject(err)
+      else resolve(data.Body)
+    })
   })
-  console.log(listFiles('/tmp'))
-  // console.log('Here')
-  // process.exit(1)
 }
 
 const saveEventFeatureToFile = (event) => {
@@ -138,33 +144,39 @@ const saveEventFeatureToFile = (event) => {
   }
   // console.log('Feature: ' + feature)
   fs.writeFileSync(localFeatureFilename, feature, 'utf8')
-  // console.log('Saved input feature to ' + localFeatureFilename)
   const command = 'cat'
   const args = [ localFeatureFilename ]
   const featureFileContent = shellExec(command, args)
   console.log('featureFileContent: \n' + featureFileContent.output)
-  // console.log('localFeatureFilename: ' + localFeatureFilename)
   return localFeatureFilename
 }
 
-const executeCucumber = (featureFilename) => {
+const executeCucumber = async (featureFilename) => {
   // const tmpFiles = listFiles('/tmp')
-  // console.log('files: ' + tmpFiles)
+  console.log('/tmp files prior to executeCucumber: ' + listFiles('/tmp'))
+  console.log('featureFilename: ' + featureFilename)
   const cmd = './cucumber-js'
-  // const args = [featureFilename, '--format', 'json', '--require', '"/tmp/step-definitions/*.js"']
-  const args = [featureFilename, '--format', 'json', '--require', '"/var/task/features/**/*.js"']
+  const args = [featureFilename, '--format', 'json', '--require', '"/tmp/**/*.js"']
   // const args = [featureFilename, '--format', 'json']
-  // const args = [featureFilename, '--format', 'json', '--require', '\'../../features/**/*.js\'']
 
+  // console.log(listFiles('/var/task/node_modules/cucumber/bin'))
+  console.log(listFiles('./node_modules/cucumber/bin'))
   console.log('Cucumber request: \n' + cmd + ' ' + JSON.stringify(args).replace(/,/g, ' '))
-  const result = shellExec(cmd, args, './node_modules/cucumber/bin')
-  console.log('Cucumber response: \n' + result.output)
-  return result
+  // const result = await shellExec(cmd, args, './node_modules/cucumber/bin')
+  const result = await shell.spawnSync(cmd, args, { cwd: './node_modules/cucumber/bin', env: process.env, stdio: 'pipe', encoding: 'utf-8' })
+  // const result = await shell.execSync('./cucumber-js ' + featureFilename + ' --format json --require /tmp/step-definitions/*.js',
+  //   { cwd: '.node_modules/cucumber/bin', env: process.env, stdio: 'pipe', encoding: 'utf-8' })
+  console.log('Cucumber response: \n' + JSON.stringify(result))
+  // process.exit(1)
+  const output = result.stdout
+  const status = result.status
+  return { status, output }
 }
 
 const listFiles = (directory) => {
   const cmd = 'ls'
   const args = ['-lR', directory + '/']
+  console.log('Requesting directory listing for ' + directory)
   const files = shellExec(cmd, args)
   return files.output
 }
@@ -180,8 +192,18 @@ const shellExec = (cmd, args, cwd) => {
   if (cwd === undefined) {
     cwd = process.cwd()
   }
-  const result = shell.spawnSync(cmd, args, { cwd: cwd, env: process.env, stdio: 'pipe', encoding: 'utf-8' })
+  const result = shell.spawnSync(cmd, args, { cwd: cwd, env: process.env, stdio: 'pipe', encoding: 'utf-8', shell: true })
+  // console.log('result.stderr: ' + result.stderr)
+  // console.log('result.stdout: ' + result.stdout)
+  // console.log('result.status: ' + result.status)
+  // console.log('result.error: ' + result.error)
   const output = result.stdout
   const status = result.status
   return { status, output }
 }
+
+// const runCucumber = (args) => {
+//   const cliArgs = { argv: args, cwd: process.cwd(), stdout: process.stdout }
+//   let cli = (new require('cucumber').Cli)(cliArgs)
+//   cli.run() // Returns a promise
+// }
